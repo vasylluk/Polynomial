36029e6ab0ce5a425066b5a6b6b8339bd68eb613
vasylluk <vasyl3luk@gmail.com>
2018-06-12 21:44:40 +0300

    Initial commit

---
+++ ConsoleApp4/Polynomial.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Monomial
+    {
+        int degree;
+        double coeficient;
+        public int Degree
+        {
+            get => degree;
+            set
+            {
+                degree = value;
+            }
+        }
+
+        public double Coeficient
+        {
+            get => coeficient;
+            set
+            {
+                coeficient = value;
+            }
+        }
+
+        public Monomial(int degree, double coeficient)
+        {
+            Degree = degree;
+            Coeficient = coeficient;
+        }
+        public Monomial() : this(0,1) { }
+    }
+
+    public class Polynomial : Monomial, ICloneable, IEquatable<Polynomial>, IEnumerable<Polynomial>
+    {
+        int degree;
+
+        public double[] Coeficients { get; set; }
+        public double FreeCoef { get; set; }
+        public int Degree
+        {
+            get => degree;
+            set
+            {
+                degree = value;
+            }
+        }
+
+        public Polynomial(int degree, double[] coefs, double free)
+        {
+            Degree = degree;
+            Coeficients = coefs;
+            FreeCoef = free;
+        }
+
+        public Polynomial() : this(0, new double[0], 1) { }
+
+        public List<double> ToArray()
+        {
+            int l = Coeficients.Length + 3;
+            List<double> arr = new List<double>();
+            foreach (var item in Coeficients)
+            {
+                arr.Add(item);
+            }
+            arr.Add(FreeCoef);
+            return arr;
+        }
+        public object Clone() =>
+            new Polynomial(Degree, Coeficients, FreeCoef);
+        
+        public double MaxCoeficient()
+        {
+            double max = Double.MinValue;
+            for(int i = 0; i < Coeficients.Length; i++)
+            {
+                if (Coeficients[i] > max) max = Coeficients[i];
+            }
+            return (max>FreeCoef)? max:FreeCoef;
+        }
+
+        public double Point(double x)
+        {
+            double res = 0;
+            for(int i = 0; i < Coeficients.Length; i++)
+            {
+                res += Math.Pow(x,(Degree-i)) * Coeficients[i];
+            }
+            return res + FreeCoef;
+        }
+
+        public bool Equals(Polynomial other)
+        {
+            if (Degree != other.Degree) return false;
+            bool t = true;
+            double def = Coeficients[0] / other.Coeficients[0];
+            for (int i = 1; i < Coeficients.Length; i++)
+            {
+                if (Coeficients[i] / other.Coeficients[i] != def)
+                    t = false;
+            }
+            if (t) return true;
+            else return false;
+        }
+
+        public int CompareTo(Polynomial other)
+        {
+            var f = this.Degree - other.Degree;
+            return (f > 0) ? 1 : (f != 0) ? -1 : 0;
+        }
+
+        public IEnumerator<Polynomial> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator +(Polynomial f1, Polynomial f2)
+        {
+            int l = Math.Max(f1.Coeficients.Length, f2.Coeficients.Length);
+            int d = f1.Degree - f2.Degree;
+            double free = f1.FreeCoef + f2.FreeCoef;
+            double[] c = new double[l];
+            for (int i = 0; i < (l - d-1); i++)
+            {
+                c[i] = f1.Coeficients[i] + f2.Coeficients[i];
+            }
+            if (f1.Degree >= f2.Degree)
+            {
+                for (int i = l - d; i < l; i++)
+                {
+                    c[i] = f1.Coeficients[i];
+                }
+            }
+            else
+            {
+                for (int i = l - d; i < l; i++)
+                {
+                    c[i] = f2.Coeficients[i];
+                }
+            }
+            Polynomial res = new Polynomial
+            {
+                FreeCoef = free,
+                Degree = l,
+                Coeficients = c
+            };
+            return res;
+        }
+
+        public static bool operator ==(Polynomial f1, Polynomial f2)
+        {
+            double e = 0.0000000001;
+            int def = Math.Abs(f1.Degree - f2.Degree);
+            int n = Math.Max(f1.Degree, f2.Degree) - 1;
+            for (int i = 0; i < n - def; i++)
+            {
+                if (Math.Abs(f1.Coeficients[i] - f2.Coeficients[i + def])>2*e) return false;
+            }
+            return true;
+        }
+
+        public static bool operator !=(Polynomial f1, Polynomial f2)
+        {
+            return !(f1==f2);
+        }
+
+        public static Polynomial operator /(Polynomial f1, Monomial f2)
+        {
+            int degree = f1.Degree - f2.Degree;
+            double[] coefs = new double[f1.Degree];
+            for (int i = 0; i < f1.Coeficients.Length; i++)
+            {
+                coefs[i] = f1.Coeficients[i] / f2.Coeficient;
+            }
+            return new Polynomial()
+            {
+                Degree = degree,
+                Coeficients = coefs,
+                FreeCoef = f1.FreeCoef / f2.Coeficient
+            };
+        }
+
+        public static Polynomial operator -(Polynomial f1, Polynomial f2)
+        {
+            int l = Math.Max(f1.Coeficients.Length, f2.Coeficients.Length);
+            int d = f1.Degree - f2.Degree;
+            double free = f1.FreeCoef - f2.FreeCoef;
+            double[] c = new double[l];
+            for (int i = 0; i < l - d; i++)
+            {
+                c[i] = f1.Coeficients[i] - f2.Coeficients[i];
+            }
+            if (f1.Degree >= f2.Degree)
+            {
+                for (int i = l - d; i < l; i++)
+                {
+                    c[i] = f1.Coeficients[i];
+                }
+            }
+            else
+            {
+                for (int i = l - d; i < l; i++)
+                {
+                    c[i] = f2.Coeficients[i];
+                }
+            }
+            Polynomial res = new Polynomial
+            {
+                FreeCoef = free,
+                Degree = l,
+                Coeficients = c
+            };
+            return res;
+        }
+
+        public static Polynomial operator *(Polynomial f1, Polynomial f2)
+        {
+            int l = Math.Max(f1.Coeficients.Length, f2.Coeficients.Length);
+            double[] c = new double[l];
+            int i = 0;
+            foreach (var item in f1.Coeficients)
+            {
+                foreach (var jtem in f2.Coeficients)
+                {
+                    if (!(item == 0 || jtem == 0))
+                    {
+                        c[i] += item * jtem;
+                    }
+                    else
+                    {
+                        c[i] += item > 0 ? item : jtem > 0 ? jtem : 0;
+                    }
+                }
+                i++;
+            }
+            Polynomial res = new Polynomial
+            {
+                Degree = l,
+                FreeCoef = f1.FreeCoef * f2.FreeCoef,
+                Coeficients = c
+            };
+            return res;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp4.Polynomial;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Let's test:");
+            var f1 = new Polynomial(3, new double[] { 4, 3, 5 }, 2);
+            var f2 = new Polynomial(3, new double[] { 12, 9, 15 }, 6);
+            var m1 = new Monomial(1, 3);
+            var f4 = (f1 / m1).ToArray();
+            Console.WriteLine($"f4 Coeficients: ");
+            for (int i = 0; i < f4.Count; i++)
+            {
+                Console.Write("{0:F3}\t", f4[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"f1 value in point 3: {f1.Point(3)}");
+            Console.WriteLine($"f1 MaxCoeficient: {f1.MaxCoeficient()}");
+            Console.WriteLine();
+            Console.WriteLine($"f1 equals f2?: {f1.Equals(f2)}");
+            Console.WriteLine($"f1 = f2?: {f1 == f2}");
+            f1.Degree = 4;
+            var f3 = (f2 + f1).ToArray();
+            Console.WriteLine();
+            Console.WriteLine($"Degree of f1 = {f1.Degree}\n");
+            Console.WriteLine();
+            Console.Write($"Sum of f1 and f2 = "+Convert.ToChar(123)+" ");
+            var n = f3.Count - 1;
+            foreach (var item in f3)
+            {       
+                if (n>1)
+                Console.Write('('+Convert.ToString(item) + "x^"+Convert.ToString(n)+") + ");
+                n--;
+            }
+
+            Console.Write('(' + Convert.ToString(f3[f3.Count-2]) + "x) + ");
+            Console.Write('(' + Convert.ToString(f3[f3.Count-1]) + ") ");
+            Console.WriteLine(Convert.ToChar(125));
+            Console.ReadKey();
+        }
+    }
+}
